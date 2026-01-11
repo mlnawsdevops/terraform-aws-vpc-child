e2ad40cac6d764a92b6d10c00c55058b1e6e77ab
mlnawsdevops <mlnpublicact365@gmail.com>
2026-01-11 13:17:51 +0530

    terraform-aws-child-module

---
+++ main.tf
@@ -0,0 +1,114 @@
+resource "aws_vpc" "main" {
+    cidr_block = var.vpc_cidr #10.0.0.0/16 = 65,536 ip's
+    enable_dns_hostnames = var.enable_dns_hostnames #true
+
+    tags = merge(
+        var.common_tags,
+        var.vpc_tags,
+        {
+            Name = local.resource_name #expense-dev
+        }
+    )
+  
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+    
+    tags = merge(
+        var.common_tags,
+        var.igw_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+  
+}
+
+resource "aws_subnet" "public" {
+    count = length(var.public_subnet_cidrs) #2 availability zones
+    vpc_id = aws_vpc.main.id 
+    cidr_block = var.public_subnet_cidrs[count.index] #10.0.1.0/24, 10.0.2.0/24
+    availability_zone = local.az_names[count.index] #us-east-1a, us-east-1b
+    map_public_ip_on_launch = true #for enabling the public ip
+
+    tags = merge(
+        var.common_tags,
+        var.public_subnet_tags,
+        {
+            Name = "${local.resource_name}-public-${local.az_names[count.index]}" #expense-dev-public-us-east-1
+        }
+    )
+}
+
+resource "aws_subnet" "private" {
+    count = length(var.private_subnet_cidrs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.private_subnet_cidrs[count.index]
+    availability_zone = local.az_names[count.index]
+
+    tags = merge(
+        var.common_tags,
+        var.private_subnet_tags,
+        {
+            Name = "${local.resource_name}-private-${local.az_names[count.index]}"
+        }
+    )
+}
+
+resource "aws_subnet" "database" {
+    count = length(var.database_subnet_cidrs)
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.database_subnet_cidrs[count.index]
+    availability_zone = local.az_names[count.index]
+
+    tags = merge(
+        var.common_tags,
+        var.database_subnet_tags,
+        {
+            Name = "${local.resource_name}-database-${local.az_names[count.index]}"
+        }
+    )
+}
+
+# DB subnet group for RDS
+resource "aws_db_subnet_group" "default" {
+    name = local.resource_name
+    subnet_ids = aws_subnet.database[*].id
+
+    tags = merge(
+        var.common_tags,
+        var.db_subnet_group_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+  
+}
+
+resource "aws_eip" "nat" {
+    domain = "vpc"
+    
+    tags = merge(
+        var.common_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+}
+
+resource "aws_nat_gateway" "main" {
+    allocation_id = aws_eip.nat.id
+    subnet_id = aws_subnet.public[0].id
+
+    tags = merge(
+        var.common_tags,
+        var.nat_gateway_tags,
+        {
+            Name = local.resource_name
+        }
+    )
+
+    depends_on = [ aws_internet_gateway.main ]
+  
+}
---
+++ variables.tf
@@ -0,0 +1,79 @@
+variable "project_name" {
+    type = string
+
+}
+
+variable "environment" {
+    type = string
+
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+    default = true
+}
+
+#optional
+variable "common_tags" {
+    type = map
+
+}
+
+#optional
+variable "vpc_tags" {
+    type = map
+
+}
+
+variable "igw_tags" {
+    type = map
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list 
+    validation {
+      condition = length(var.public_subnet_cidrs) == 2
+      error_message = "Please provilde only 2 valid public subnet cidrs"
+    }
+  
+}
+
+variable "public_subnet_tags" {
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "Please provide only 2 valid private subnet cidrs"
+    }
+}
+
+variable "private_subnet_tags" {
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "Please provide only 2 valid database subnet CIDRs"
+    }
+}
+
+variable "database_subnet_tags" {
+    default = {}
+}
+
+variable "db_subnet_group_tags" {
+    default = {}
+}
+
+variable "nat_gateway_tags" {
+    default = {}
+}
